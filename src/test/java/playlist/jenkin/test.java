130856babd3ff3fe2850ee7172d904ec7fd8bbd8
deepak sihag <sihag.deepak@gmail.com>
2014-07-05 22:50:37 +0530

    jacoco
    add jacoco plugin to check report in jenkins

---
+++ src/test/java/playlist/jenkin/test.java
@@ -0,0 +1,15 @@
+package playlist.jenkin;
+ 
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+ 
+public class TestBankAccount {
+
+@Test
+public void testJenkinsJacocoReport(){
+Assert.assertEquals(5,5);
+
+}
+ 
+}
